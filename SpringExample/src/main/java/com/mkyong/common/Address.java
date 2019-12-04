fce284cda2f054a7ad28e55187f1ec9d841d9d74
G.ASHOK <G.ASHOK@LAPTOP-9339BM61>
2019-12-04 13:50:23 +0530

    Spring example

---
+++ SpringExample/src/main/java/com/mkyong/common/Address.java
@@ -0,0 +1,29 @@
+package com.mkyong.common;
+
+public class Address {
+	
+	private int id;
+	private String area;
+	private String userId;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	
+
+}
---
+++ SpringExample/src/main/java/com/mkyong/common/WelcomeBean.java
@@ -0,0 +1,19 @@
+package com.mkyong.common;
+
+public class WelcomeBean {
+	
+	private String message;
+
+	   public void setMessage(String message)
+	   {
+		   this.message = message;
+	   }
+
+	   public void show()
+	   {
+		   System.out.println(message);
+	   }
+
+	}
+
+
